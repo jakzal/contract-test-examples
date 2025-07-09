670ab231d03fe1f6449158dd5af13555c21ac5f4
Jakub Zalas <jakub@zalas.pl>
2025-07-09 20:57:00 +0100

    [trading-composition][Kotlin] Create the exposed adapter for the database implementation of the TradingOrderRepository

---
+++ kotlin/trading-composition/adapters/exposed/src/main/kotlin/trading/adapters/exposed/TradeOrders.kt
@@ -0,0 +1,16 @@
+package trading.adapters.exposed
+
+import org.jetbrains.exposed.v1.core.Table
+import trading.TradeOrderStatus
+import trading.TradeOrderType
+
+object TradeOrders : Table("trade_orders") {
+    private val id = integer("id").autoIncrement()
+    override val primaryKey = PrimaryKey(id)
+    val trackingId = text("tracking_id").uniqueIndex()
+    val brokerageAccountId = text("brokerage_account_id").index()
+    val type = enumeration("trade_order_type", TradeOrderType::class).index()
+    val security = text("trade_order_security").index()
+    val numberOfShares = integer("number_of_shares")
+    val status = enumeration("trade_order_status", TradeOrderStatus::class).index()
+}
